1cd8abf2185f0b7a9340b445a76d547ab98dd8e8
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-11-22 12:19:51 -0500

    Created the basic exporters for the Name and Text types

--- engines/Unreal/Plugins/PokeSharp/Source/PokeSharpCore/Public/Interop/PokeSharpNameExporter.h
+++ engines/Unreal/Plugins/PokeSharp/Source/PokeSharpCore/Public/Interop/PokeSharpNameExporter.h
@@ -1,18 +1,33 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
+#include "CSBindsManager.h"
 #include "UObject/Object.h"
+
 #include "PokeSharpNameExporter.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class POKESHARPCORE_API UPokeSharpNameExporter : public UObject
 {
-	GENERATED_BODY()
-	
-	
+    GENERATED_BODY()
+
+  public:
+    UNREALSHARP_FUNCTION()
+    static void GetOrAddEntry(const UTF16CHAR *Str, int32 Length, bool FindMode, uint32 &ComparisonIndex,
+                              uint32 &DisplayIndex, int32 &Number);
+
+    UNREALSHARP_FUNCTION()
+    static bool EqualsBuffer(uint32 ComparisonIndex, uint32 DisplayIndex, int32 Number, const UTF16CHAR *Str,
+                             int32 Length);
+
+    UNREALSHARP_FUNCTION()
+    static void GetString(uint32 ComparisonIndex, uint32 DisplayIndex, int32 Number, FString &OutString);
+
+  private:
+    static FName GetName(uint32 ComparisonIndex, uint32 DisplayIndex, int32 Number);
 };

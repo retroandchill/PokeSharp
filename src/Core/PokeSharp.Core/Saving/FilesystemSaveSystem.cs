3fa75dabd8d0b674fc8efd2eaa5ca558fdc5e5b0
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-11-23 10:52:13 -0500

    Added the filesystem abstraction to SaveSystem as well

--- src/Core/PokeSharp.Core/Saving/FilesystemSaveSystem.cs
+++ src/Core/PokeSharp.Core/Saving/FilesystemSaveSystem.cs
@@ -1,29 +1,30 @@
-﻿using Microsoft.Extensions.Options;
+﻿using System.IO.Abstractions;
+using Microsoft.Extensions.Options;
 using Zomp.SyncMethodGenerator;
 
 namespace PokeSharp.Core.Saving;
 
 [RegisterSingleton]
-public partial class FilesystemSaveSystem(IOptionsMonitor<SaveDataConfig> config) : ISaveSystem
+public partial class FilesystemSaveSystem(IFileSystem fileSystem, IOptionsMonitor<SaveDataConfig> config) : ISaveSystem
 {
     public bool Exists(string filePath)
     {
-        return File.Exists(Path.Join(config.CurrentValue.SaveFilePath, filePath));
+        return fileSystem.File.Exists(Path.Join(config.CurrentValue.SaveFilePath, filePath));
     }
 
     public Stream OpenRead(string filePath)
     {
-        return File.OpenRead(Path.Join(config.CurrentValue.SaveFilePath, filePath));
+        return fileSystem.File.OpenRead(Path.Join(config.CurrentValue.SaveFilePath, filePath));
     }
 
     public Stream OpenWrite(string filePath)
     {
-        if (!Directory.Exists(config.CurrentValue.SaveFilePath))
+        if (!fileSystem.Directory.Exists(config.CurrentValue.SaveFilePath))
         {
-            Directory.CreateDirectory(config.CurrentValue.SaveFilePath);
+            fileSystem.Directory.CreateDirectory(config.CurrentValue.SaveFilePath);
         }
 
-        return File.OpenWrite(Path.Join(config.CurrentValue.SaveFilePath, filePath));
+        return fileSystem.File.OpenWrite(fileSystem.Path.Join(config.CurrentValue.SaveFilePath, filePath));
     }
 
     [CreateSyncVersion]
@@ -33,9 +34,9 @@ public partial class FilesystemSaveSystem(IOptionsMonitor<SaveDataConfig> config
         CancellationToken cancellationToken = default
     )
     {
-        File.Copy(
-            Path.Join(config.CurrentValue.SaveFilePath, sourceFilePath),
-            Path.Join(config.CurrentValue.SaveFilePath, destinationFilePath)
+        fileSystem.File.Copy(
+            fileSystem.Path.Join(config.CurrentValue.SaveFilePath, sourceFilePath),
+            fileSystem.Path.Join(config.CurrentValue.SaveFilePath, destinationFilePath)
         );
         return ValueTask.CompletedTask;
     }
@@ -43,7 +44,7 @@ public partial class FilesystemSaveSystem(IOptionsMonitor<SaveDataConfig> config
     [CreateSyncVersion]
     public ValueTask DeleteAsync(string filePath, CancellationToken cancellationToken = default)
     {
-        File.Delete(Path.Join(config.CurrentValue.SaveFilePath, filePath));
+        fileSystem.File.Delete(fileSystem.Path.Join(config.CurrentValue.SaveFilePath, filePath));
         return ValueTask.CompletedTask;
     }
 }

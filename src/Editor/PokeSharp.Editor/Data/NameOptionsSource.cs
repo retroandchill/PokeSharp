8851bf44abdef15027cf0123228f197ac94c494b
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-11-27 10:46:19 -0500

    Added an API to get all the options for a specific type as necessary

---
+++ src/Editor/PokeSharp.Editor/Data/NameOptionsSource.cs
@@ -0,0 +1,15 @@
+﻿using PokeSharp.Core.Data;
+using PokeSharp.Core.Strings;
+
+namespace PokeSharp.Editor.Data;
+
+internal interface INameOptionsSource
+{
+    (int Count, IEnumerable<Name> Names) Options { get; }
+}
+
+internal sealed class NameOptionsSource<T> : INameOptionsSource
+    where T : IGameDataEntity<Name, T>
+{
+    public (int Count, IEnumerable<Name> Names) Options => (T.Count, T.Keys);
+}
---
+++ src/Editor/PokeSharp.Editor/Data/SpeciesOptionSource.cs
@@ -0,0 +1,17 @@
+﻿using PokeSharp.Core.Strings;
+using PokeSharp.Data.Pbs;
+
+namespace PokeSharp.Editor.Data;
+
+public class SpeciesOptionSource : INameOptionsSource
+{
+    public (int Count, IEnumerable<Name> Names) Options
+    {
+        get
+        {
+            var speciesKeys = Species.Keys.Where(x => x.Form == 0).Select(x => x.Species).ToArray();
+
+            return (speciesKeys.Length, speciesKeys);
+        }
+    }
+}
